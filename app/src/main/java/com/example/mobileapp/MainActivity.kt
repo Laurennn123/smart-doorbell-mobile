a30f5c278f3481b9771ad81b4039ff9463207519
Laurennn123 <2021-100715@rtu.edu.ph>
2024-12-19 21:02:27 +0800

    draft UI homepage

--- app/src/main/java/com/example/mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -29,20 +30,27 @@ import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.rounded.Done
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -62,8 +70,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     containerColor = Color(0xfff5efe7)  //for setting the color of background
                 ) {
-                    BackAndUserButton(Modifier.fillMaxWidth().fillMaxSize().offset(y = 60.dp))
-                    Settings(Modifier.fillMaxSize().padding(start = 25.dp, end = 25.dp).offset(y = -80.dp))
+                    // draft Settings ui but not interactive when its clicked
+//                    BackAndUserButton(Modifier.fillMaxWidth().fillMaxSize().offset(y = 60.dp))
+//                    Settings(Modifier.fillMaxSize().padding(start = 25.dp, end = 25.dp).offset(y = -80.dp))
+
+                    // draft ui in Home page
+                    HeaderHomePage("Borilla", Modifier.fillMaxSize().fillMaxWidth().padding(start = 15.dp, end = 15.dp).offset(y = 55.dp))
+                    BodyHomePage(Modifier.fillMaxSize().padding(start = 15.dp, end = 15.dp).offset(y = 120.dp))
                 }
             }
         }
@@ -177,34 +190,29 @@ fun Contacts(contactNum: String, username: String, email: String, modifier: Modi
 
 // starts here the customizing of the settings
 @Composable
+fun IconButtonIcon(modifier: Modifier = Modifier, icon: ImageVector, iconModifier: Modifier = Modifier, onClick: () -> Unit) {
+    IconButton(
+        modifier = modifier,
+        onClick = { onClick }
+    ) {
+        Icon(
+            modifier = iconModifier,
+            imageVector = icon,
+            contentDescription = null,
+        )
+    }
+}
+
+@Composable
 fun BackAndUserButton(modifier: Modifier = Modifier) {
     Row(
         modifier = modifier,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        IconButton(
-            onClick = { },
-            enabled = true,
-            modifier = Modifier.padding(start = 5.dp)
-        ) {
-            Icon(
-                Icons.AutoMirrored.Filled.ArrowBack,
-                contentDescription = null
-            )
-        }
-        IconButton(
-            onClick = { },
-            enabled = true,
-            modifier = Modifier.padding(end = 15.dp)
-        ) {
-            Icon(
-                Icons.Filled.AccountCircle,
-                contentDescription = null
-            )
-        }
+        IconButtonIcon(modifier = Modifier.padding(start = 5.dp), icon = Icons.AutoMirrored.Filled.ArrowBack, onClick = {})
+        IconButtonIcon(modifier = Modifier.padding(end = 15.dp), icon = Icons.Filled.AccountCircle, onClick = {})
     }
 }
-
 
 @Composable
 fun SettingsSelect(type: String, icon: ImageVector, modifier: Modifier = Modifier, onClick: () -> Unit) {
@@ -212,7 +220,7 @@ fun SettingsSelect(type: String, icon: ImageVector, modifier: Modifier = Modifie
         onClick = {  },
         modifier = modifier.fillMaxWidth().height(80.dp),
         shape = RoundedCornerShape(15.dp),
-        colors = ButtonDefaults.buttonColors(containerColor = Color.White.copy(alpha = 0.5f))
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Gray.copy(alpha = 0.5f))
     ) {
         Row (
             modifier = Modifier.fillMaxWidth()
@@ -230,7 +238,6 @@ fun SettingsSelect(type: String, icon: ImageVector, modifier: Modifier = Modifie
     }
 
     // soon when tapping different button
-//
 //    when (type) {
 //        "My Account" -> println("Account")
 //        "Face Enrollment" -> println("Enrollment")
@@ -239,7 +246,6 @@ fun SettingsSelect(type: String, icon: ImageVector, modifier: Modifier = Modifie
 //        "About Us" -> println("Information")
 //        "Log Out" -> println("exit")
 //    }
-
 
 }
 
@@ -264,6 +270,120 @@ fun Settings(modifier: Modifier = Modifier) {
     }
 }
 
+// starts of home page
+@Composable
+fun HeaderHomePage(nameOfOwner: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column {
+            Text(text = "Welcome back, $nameOfOwner", fontWeight = FontWeight.Bold)
+            Text(text = "Check your front door!", fontWeight = FontWeight.Bold)
+        }
+        Row {
+            IconButtonIcon(icon = Icons.Filled.Menu,  modifier = Modifier.offset(x = 15.dp), onClick = {})
+            IconButtonIcon(icon = Icons.Filled.AccountCircle, onClick = {})
+        }
+    }
+}
+
+@Composable
+fun BodyHomePage(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+    ) {
+        Column {
+            Text(text = "Recent Notification", fontWeight = FontWeight.Bold, fontSize = 16.sp)
+            Button(
+                modifier = Modifier.height(60.dp),
+                shape = RoundedCornerShape(20.dp),
+                onClick = { }
+            ) {
+                Row(
+                    Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Icon(
+                        Icons.Filled.Notifications,
+                        contentDescription = null
+                    )
+                    Text(
+                        modifier = Modifier.padding(start = 10.dp, top = 3.dp),
+                        text = "Someone is at your front door !",
+                        fontSize = 15.sp
+                    )
+                }
+            }
+        }
+
+        Column(
+            Modifier.padding(top = 8.dp)
+        ){
+            Text(text = "Camera", fontWeight = FontWeight.Bold)
+            Box(
+                modifier = Modifier.fillMaxWidth().background(Color.Gray, shape = RoundedCornerShape(20.dp)).height(90.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(onClick = { }) {
+                    Text(text = "Live View")
+                }
+            }
+        }
+
+        Column(
+            Modifier.padding(top = 8.dp, bottom = 12.dp),
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Text(text = "Actions", fontWeight = FontWeight.Bold, fontSize = 16.sp)
+            Button(onClick = { }) {
+                Row(
+                    Modifier.fillMaxWidth().padding(start = 10.dp),
+                    horizontalArrangement = Arrangement.Start
+                ) { Text(text = "Unlock Door") }
+            }
+            Button(onClick = { }) {
+                Row(
+                    Modifier.fillMaxWidth().padding(start = 10.dp),
+                    horizontalArrangement = Arrangement.Start
+                ) { Text(text = "Alarm Trigger") }
+            }
+        }
+
+        Box(
+            modifier = Modifier.fillMaxWidth().background(Color.Gray, shape = RoundedCornerShape(20.dp)).height(130.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(modifier = Modifier.align(Alignment.Start).offset(x = 15.dp).padding(top = 5.dp, bottom = 5.dp), text = "Message Panel", fontWeight = FontWeight.Bold, fontSize = 13.sp)
+                var text by remember { mutableStateOf("") }
+                TextField(
+                    value = text,
+                    onValueChange = { text = it},
+                    modifier = Modifier.fillMaxWidth().height(50.dp).padding(start = 35.dp, end = 35.dp)
+                )
+                Row(
+                    modifier = Modifier.align(alignment = Alignment.End).offset(x = -32.dp).padding(top = 5.dp,  bottom = 5.dp)
+                ) {
+                    Button(onClick = { }, modifier = Modifier.height(29.dp)) {
+                        Text("Clear", fontSize = 9.sp, modifier = Modifier.offset(y = -4.dp))
+                    }
+                    Button(onClick = { }, modifier = Modifier.height(29.dp).padding(start = 10.dp)) {
+                        Text("Enter", fontSize = 9.sp, modifier = Modifier.offset(y = -4.dp))
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+// create a function for icon button and icon
+
+
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun Test() {
@@ -273,7 +393,13 @@ fun Test() {
 //        Introduce("Lauren Andre David", "Web Developer")
 //        Contacts("09521478934", "@Laurennn123", "meetmeat32@gmail.com")
 
-        BackAndUserButton(Modifier.fillMaxWidth().fillMaxSize().offset(y = 60.dp))
-        Settings(Modifier.fillMaxSize().padding(start = 25.dp, end = 25.dp).offset(y = -140.dp))
+
+        // draft Settings ui but don't have function when its clicked
+//        BackAndUserButton(Modifier.fillMaxWidth().fillMaxSize().offset(y = 60.dp))
+//        Settings(Modifier.fillMaxSize().padding(start = 25.dp, end = 25.dp).offset(y = -30.dp))
+
+        // draft Home Page ui but don't have function when its clicked
+        HeaderHomePage("Borilla", Modifier.fillMaxSize().fillMaxWidth().padding(start = 15.dp, end = 15.dp).offset(y = 55.dp))
+        BodyHomePage(Modifier.fillMaxSize().padding(start = 15.dp, end = 15.dp).offset(y = 120.dp))
     }
 }
